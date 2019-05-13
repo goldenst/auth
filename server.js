6f9c2599f7670afed3c657b12565f0f9ebbf4323
goldenst <jeff@goldenstatetow.com>
2019-05-13 16:06:00 -0700

    commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require ('express');
+const connectDB = require ('./config/db')
 
 const app = express();
 
+// connect Database
+//connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
 // test script for postman
 app.get('/', (req, res) => res.send('API Running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+//app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
